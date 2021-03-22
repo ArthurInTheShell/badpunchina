9b673f4eca410af1fdb74461a91a6dfbce785772
Junhao "Arthur" Chen <junhaochenarthur@outlook.com>
2021-03-22 16:30:54 -0400

    added querying functionality on the server side

--- server.py
+++ server.py
@@ -1,15 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from hashlib import sha256
+from flask_restful import Resource, Api,reqparse
 
 app = Flask(__name__)
-chain = []
+chain = {
+            'index':'0',
+            'block':[],
+        }
+api = Api(app)
+parser = reqparse.RequestParser()
+pendingTransaction = []
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
-@app.route('/chain')
-def printChain():
-    return len(chain)
+class Chain(Resource):
+    def get(self):
+        return chain
 
+class Transactions(Resource):
+    def get(self):
+        return jsonify(pendingTransaction)
 
+class NewTransaction(Resource):
+    def post(self):
+        args = request.get_data()
+        pendingTransaction.append(args)
+        resp = jsonify(success=True)
+        resp.status_code = 200
+        return resp
+
+api.add_resource(Chain,'/chain')
+api.add_resource(Transactions,'/transactions')
+api.add_resource(NewTransaction,'/transactions/new')
 app.run()
